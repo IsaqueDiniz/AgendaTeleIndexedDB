568ef741ef97dc2bc4f4f5d7a3af0a745adf8cce
IsaqueDiniz <isaquediniz3713@gmail.com>
2018-06-29 14:05:53 -0300

    Finalização

--- src/js/main.js
+++ src/js/main.js
@@ -15,6 +15,8 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 		inputs 	= document.getElementsByClassName('input'), /*Pega todos inputs*/
 		alertas = document.getElementsByClassName('Alert'), /*Pega as mensagens*/
 		sendBTN = document.getElementById('addBTN');
+
+	let globalTableLinhas;
 					
 					for ( let i = 0; i < inputs.length; i++) {
 						inputs[i].addEventListener('blur', function (event) { /*Começa a fazer a avaliação quando o usuário começa a digitar*/
@@ -64,7 +66,7 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 					tx.oncomplete = onTransactionSuccess //Quando a transação é bem sucedida mostra um aviso 
 
 					tx.onerror = function (event) {
-						console.log('Erro ao adicionar ao banco');
+						console.log('Erro ao adicionar no banco');
 						alert('Ocorreu um erro ao adicionar ao banco. Erro: ' + event.target.errorCode)
 					};
 					let objectStore = tx.objectStore('Contatos');
@@ -72,10 +74,14 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 					GetResources(); /*Chama a função para atualizar a view*/
 				};
 			GetResources(); /*Chama essa função para carregar view com os dados ao iniciar aplicativo*/
-		});	
+		});
+
+
+			
 
 	function GetResources (event) { /*Essa função recupera os dados do banco*/
 				const tabela = document.getElementById('contatosView'); 
+				globalTable = tabela; /*Atribui a tabela para manipulação futuras*/
 				tabela.innerHTML = ''; /*reseta a tabela para quando for adicionado novos dados não se repetir os antigos*/
 				let Get = db.transaction(['Contatos'], 'readwrite').objectStore('Contatos'); /*Cria transação 'get'*/
 
@@ -84,37 +90,42 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 					let state = ''; /*variavel para ser somada ao inner da tabela*/
 						if(cursor) {
 							console.log(cursor.value.nome + ' recuperado com sucesso');
-							state += '<tr>';
-							state += 	'<td class="tindex">' + cursor.key + '</td>';
-							state += 	'<td class="tnome">' + cursor.value.nome + '</td>';
-							state +=   '<td class="ttelefone">' + cursor.value.telefone + '</td>';
-							state +=   '<td class="temail">' + cursor.value.email + '</td>';
-							state +=   '<td class="texcluir" onclick="deletarContato('+cursor.key+'), GetResources()"' + '>' + ' X ' + '</td>';
+							state += '<tr class="row '+ cursor.key +'" id="row'+ cursor.key +'">';
+							state +=   '<td class="tindex" id="tindex' + cursor.key +'">' + cursor.key + '</td>';
+							state +=   '<td class="tnome" id="tnome'+ cursor.key +'">' + cursor.value.nome + '</td>';
+							state +=   '<td class="ttelefone" id="ttelfone'+ cursor.key +'">' + cursor.value.telefone + '</td>';
+							state +=   '<td class="temail" id="temail'+ cursor.key +'"">' + cursor.value.email + '</td>';
+							state +=   '<td class="texcluir" id="texcluir'+ cursor.key +'" onclick="deletarContato('+cursor.key+'), GetResources()"' + '>' + ' X ' + '</td>';
 							state += '</tr>' ;
 							cursor.continue();
 						}else { 
 							console.log('Terminada recuperação de dados');
 						};
 					tabela.innerHTML += state; /*adiciona os dados no final do loop*/
-				}; 
-
+				};
 			};
 
 	function deletarContato (contatoKey) {
+		const alerta = document.getElementById('deleteMessage');
 		let deleteOperation = db.transaction(['Contatos'], 'readwrite').objectStore('Contatos').delete(contatoKey); /*usa o argumento que será passado para deletar X contato*/
-
 			deleteOperation.onsuccess = function () {
 				console.log('deletado com sucesso');
+				alerta.style.visibility = 'visible';
+				alerta.style.opacity = '1';
+				setTimeout(function() {
+					alerta.style.visibility = 'hidden';
+					alerta.style.opacity = '0';				
+				}, 1000);
 			};
 	};
 
 	function openMsg () {
-		const alert = document.getElementById('openMessage');
-		alert.style.visibility = 'visible';
-		alert.style.opacity = '1';
+		const alerta = document.getElementById('openMessage');
+		alerta.style.visibility = 'visible';
+		alerta.style.opacity = '1';
 		setTimeout(function() {
-			alert.style.opacity = '0';
-			alert.style.visible = 'hidden';
+			alerta.style.opacity = '0';
+			alerta.style.visible = 'hidden';
 		}, 2000);
 	};
 
@@ -131,4 +142,4 @@ window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndex
 			};
 		sendBTN.disabled = true; /*Desabilita o controle*/
 		sendBTN.removeAttribute('class'); 	
-	}
+	};
